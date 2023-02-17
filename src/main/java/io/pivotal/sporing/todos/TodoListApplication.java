6313213e234d1fe363b094c4b81a3aeeb6ddbfed
sungsoyeon <sos980621@naver.com>
2023-02-17 16:17:20 +0900

    0217_1617

--- src/main/java/io/pivotal/sporing/todos/TodoListApplication.java
+++ src/main/java/io/pivotal/sporing/todos/TodoListApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoListApplication {
-
+	//0217 test
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
